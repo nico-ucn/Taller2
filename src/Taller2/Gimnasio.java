5f68ff90453ff4f04c63a8488444fa5ebe8e750a
nico-ucn <nicolas.lucero@alumnos.ucn.cl>
2026-05-10 18:02:37 -0400

    Update Gimnasio.java

--- src/Taller2/Gimnasio.java
+++ src/Taller2/Gimnasio.java
@@ -1,5 +1,23 @@
 package Taller2;
 
-public class Gimnasio {
+public class Gimnasio extends Rival{
+private int numeroGimnasio;
+private int cantPokemons;
 
+
+	public Gimnasio(int numeroGimnasio,String nombre, String estado, int cantPokemons) {
+		super(nombre, estado);
+		
+		this.numeroGimnasio = numeroGimnasio;
+		this.cantPokemons = cantPokemons;
+	}
+	
+	public int getNumeroGimnasio() {
+		return this.numeroGimnasio;
+	}
+	
+	public int getCantPokemons() {
+		return this.cantPokemons;
+	}
+	
 }
