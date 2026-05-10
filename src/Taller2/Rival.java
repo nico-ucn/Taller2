212af5b4383402985f377eca36cd7bdd4b841115
nico-ucn <nicolas.lucero@alumnos.ucn.cl>
2026-05-10 18:04:17 -0400

    Update Rival.java

--- src/Taller2/Rival.java
+++ src/Taller2/Rival.java
@@ -2,4 +2,39 @@ package Taller2;
 
 public class Rival {
 
+	protected String nombre;
+	protected Pokemon[] equipo;
+	protected String estado;
+	
+	
+	public Rival(String nombre,String estado) {
+		this.nombre = nombre;
+		this.estado = estado;
+		this.equipo = new Pokemon[6];
+	}
+		
+		
+		public String getNombre() {
+			return this.nombre;
+	}
+	
+	public String getEstado(){
+		return this.estado;
+	}
+	
+	public Pokemon[] getEquipo() {
+		return this.equipo;
+	}
+	
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public void agregarPokemonAlEquipo(Pokemon p, int posicion) {
+		if (posicion >= 0 && posicion < 6) {
+			this.equipo[posicion] = p;
+		}
+	}
+	
+	
 }
