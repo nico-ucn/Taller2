499e1bffc467e129166485d42c0474def427c077
nico-ucn <nicolas.lucero@alumnos.ucn.cl>
2026-05-10 18:03:03 -0400

    Update AltoMando.java

--- src/Taller2/AltoMando.java
+++ src/Taller2/AltoMando.java
@@ -1,5 +1,17 @@
 package Taller2;
 
-public class AltoMando {
-
+public class AltoMando extends Rival{
+	
+	private int numeroMando;
+	
+	public AltoMando(int numeroMando, String nombre) {
+		super(nombre,"Sin derrotar");
+		
+		this.numeroMando = numeroMando;
+	}
+	
+	public int getNumeroMando() {
+		return this.numeroMando;
+	}
+	
 }
